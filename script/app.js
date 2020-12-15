6801dae61c836bc2e01b834132424443e69d1d5b
sarath <sarathdon9245@gmail.com>
2020-12-15 07:53:32 +0530

    🚀 finished basic layout and styling

---
+++ script/app.js
@@ -0,0 +1,10 @@
+const apiUrl = 'https://gitconnected.com/v1/portfolio/saratkumar17mss040';
+
+// getAllData(apiUrl);
+
+// function getAllData(apiUrl) {
+// 	fetch(apiUrl)
+// 		.then((res) => res.json())
+// 		.then((data) => console.log(data));
+// }
+
